fd24ce9deadc668b39f6561173b56a4849897727
Julia <juliajun@uef.fi>
2024-10-15 15:17:53 +0300

    5.1. toimii

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from 'react'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
+import './Style.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [messageType, setMessageType] = useState('')
 
   useEffect(() => {
     blogService
@@ -20,14 +28,75 @@ const App = () => {
       })
   }
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setErrorMessage('Login successful!')
+      setMessageType('success')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 3000)
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setMessageType('error')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 3000)
+    }
+  }
+
+  const blogForm = () => (
+    <ul>
+      {blogs.map(blog =>
+        <li key={blog.id}> Blogin otsikko: {blog.title}, Blogin tekijä: {blog.author}, Blogin osoite: {blog.url}, Tykkäyksiä: {blog.likes} <button type="button" onClick={() => buttonHandler(blog.id)}>Poisa</button></li>
+      )}
+    </ul>
+  )
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
   return (
     <div>
-      <h1>Hieno blokiso vellus</h1>
-      <ul>
-        {blogs.map(blog =>
-          <li key={blog.id}> Blogin otsikko: {blog.title}, Blogin tekijä: {blog.author}, Blogin osoite: {blog.url}, Tykkäyksiä: {blog.likes} <button type="button" onClick={() => buttonHandler(blog.id)}>Poisa</button></li>
-        )}
-      </ul>
+      <h1>Hieno bloki sovellus</h1>
+
+      <Notification message={errorMessage} type={messageType} />
+      <h2>Login</h2>
+      {!user && loginForm()}
+      {user && <div>
+        <p>{user.username} logged in</p>
+      </div>
+      }
+
+      {user && blogForm()}
     </div>
   )
 }
